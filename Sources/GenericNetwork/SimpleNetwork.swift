d3b93a9de3d88d9a50a912254dae9949b8c3ffdc
Alex Crowe <11133687+AlexHmelevski@users.noreply.github.com>
2023-12-26 15:45:59 -0500

    Convenince init

--- Sources/GenericNetwork/SimpleNetwork.swift
+++ Sources/GenericNetwork/SimpleNetwork.swift
@@ -2,3 +2,8 @@ import Foundation
 
 public typealias SimpleNetwork = GenericNetwork<PassthroughFactory>
 
+public extension SimpleNetwork {
+    init(configuration: URLSessionConfiguration) {
+        self.init(urlSession: .init(configuration: configuration), factory: .init())
+    }
+}
