43978cc361f6a09c7f6af0c33fcc343ca2691d9c
Alex Crowe <11133687+AlexHmelevski@users.noreply.github.com>
2023-12-28 21:12:20 -0500

    linux support

--- Sources/GenericNetwork/Adapters/ResponseAdapter.swift
+++ Sources/GenericNetwork/Adapters/ResponseAdapter.swift
@@ -1,4 +1,8 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
 
 public protocol ResponseAdapter {
     func response<T: Decodable>(for data: Data, response: URLResponse) throws -> T
