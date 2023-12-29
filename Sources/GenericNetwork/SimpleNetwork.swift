eab53ebfc3742f7bf77a5436c06d7f8aa0a74af5
Alex Crowe <11133687+AlexHmelevski@users.noreply.github.com>
2023-12-28 21:01:16 -0500

    linux support

--- Sources/GenericNetwork/SimpleNetwork.swift
+++ Sources/GenericNetwork/SimpleNetwork.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public typealias SimpleNetwork = GenericNetwork<PassthroughFactory>
 
--- Sources/GenericNetwork/Tasks/DataTask.swift
+++ Sources/GenericNetwork/Tasks/DataTask.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 struct RequestDataTask {
     private let session: URLSession
